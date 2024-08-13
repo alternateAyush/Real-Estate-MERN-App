65ff176015a25388ad44fc00833c8a9f77a0fc87
Ayush Bhatt <ayushbhatt2002@gmail.com>
2024-08-13 13:16:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Real-Estate-MERN-App
+ A real estate app made with MERN stack and tailwind css
---
+++ client/src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from './Home.jsx'
+import About from './About.jsx'
+import Profile from './Profile.jsx'
+import SignUp from './SignUp.jsx'
+import SignIn from './SignIn.jsx'
+
+export {Home,About,Profile,SignUp,SignIn};
