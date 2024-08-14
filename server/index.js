6775549d74e67ea0f79ae3141c0dfdf29ccec464
Ayush Bhatt <ayushbhatt2002@gmail.com>
2024-08-14 16:37:36 +0530

    mongoDB setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Sever running on port 3001.");
-});
+mongoose
+  .connect(process.env.DB_CONNECTION)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(3000, () => {
+      console.log("Sever running on port 3001.");
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB error:", err);
+  });
