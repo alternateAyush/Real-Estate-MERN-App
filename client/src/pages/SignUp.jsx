6d0238ecf979cd871785889c53bc3696b8d69a47
Ayush Bhatt <ayushbhatt2002@gmail.com>
2024-08-17 19:06:50 +0530

    created signin api route

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaSpinner } from "react-icons/fa";
 import { notify } from "../utils/Notify";
 import "react-toastify/dist/ReactToastify.css";
@@ -14,16 +14,35 @@ function SignUp() {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({});
   const [loading, setLoading] = useState(false);
+  const [guideline, setGuideline] = useState(null);
   useEffect(() => {});
   const handleChange = (e) => {
+    setGuideline(null);
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
     });
+    if (e.target.id == "username" || e.target.id== 'password') {
+      if (
+        (e.target.value[0] >= "A" && e.target.value <= "Z") ||
+        (e.target.value[0] >= "a" && e.target.value <= "z")
+      ) {
+        if(e.target.value.includes(' ')){
+          setGuideline("No spaces allowed in username/password.");
+        }
+      } else {
+        setGuideline("Username/Password must start with an alphabet.");
+      }
+    }
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
+    if (guideline) {
+      notify(guideline, "error");
+      setLoading(false);
+      return;
+    }
     try {
       const res = await fetch("/api/auth/signup", {
         method: "POST",
@@ -39,41 +58,40 @@ function SignUp() {
       } else {
         notify("User signup successfull.", "success");
         setLoading(false);
-        navigate('/sign-in')
+        navigate("/sign-in");
       }
     } catch (err) {
       console.log("error1: ", err);
     }
     setLoading(false);
   };
-  const handleNotify = () => {};
   return (
-    <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-center text-xl sm:text-3xl font-semibold my-7">
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-center text-xl sm:text-3xl font-semibold my-7'>
         Sign Up
       </h1>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input
           className={inputCss}
-          placeholder="Username"
-          type="text"
-          id="username"
+          placeholder='Username'
+          type='text'
+          id='username'
           onChange={handleChange}
           required
         ></input>
         <input
           className={inputCss}
-          placeholder="Email"
-          type="email"
-          id="email"
+          placeholder='Email'
+          type='email'
+          id='email'
           onChange={handleChange}
           required
         ></input>
         <input
           className={inputCss}
-          placeholder="Password"
-          type="password"
-          id="password"
+          placeholder='Password'
+          type='password'
+          id='password'
           onChange={handleChange}
           required
         ></input>
@@ -84,12 +102,13 @@ function SignUp() {
           continue with google
         </button>
       </form>
-      <div className="flex gap-1 mt-3">
+      <div className='flex gap-1 mt-3'>
         <p>Have an account?</p>
-        <Link to="/sign-in">
-          <span className="text-blue-700">Click Here.</span>
+        <Link to='/sign-in'>
+          <span className='text-blue-700'>Click Here.</span>
         </Link>
       </div>
+      {guideline && <p className='text-red-500'>{guideline}</p>}
     </div>
   );
 }
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,8 +1,12 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.util.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
-export const signup = async (req, res,next) => {
+dotenv.config();
+
+export const signup = async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
     const salt = bcryptjs.genSaltSync(12);
@@ -18,3 +22,27 @@ export const signup = async (req, res,next) => {
   }
   return;
 };
+
+export const signin = async (req, res, next) => {
+  try {
+    const { cred, password } = req.body;
+    const validUser =
+      (await User.findOne({ email: cred })) 
+      ||(await User.findOne({ username: cred }));
+    console.log(validUser);
+    if (!validUser) {
+      return next(errorHandler(404, "User not found."));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Wrong credentials."));
+    }
+    const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+    const {password:pass,...user} = validUser._doc;
+    res.cookie('access_token',token,{httpOnly:true}).status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// node -e "console.log(require('crypto').randomBytes(32).toString('hex'))"
